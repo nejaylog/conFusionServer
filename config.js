803e46373b1d46fd2bd1dd11d4f3519a98a1afca
Hunchul Jung <nejaylog@gmail.com>
2017-12-13 18:01:03 +0900

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1587034664696811',
+        clientSecret: '74455025bb3a111037dc800d7458798b'
+    }
 };
